6edc1f81e495bab0441b8254490d91166b56d4c2
chaitanya chawak <chaitanyachawak001@gmail.com>
2025-04-14 13:53:35 +0200

    minor argname change

--- src/example_run.py
+++ src/example_run.py
@@ -8,7 +8,7 @@ catalog_type = 'PROXYSHEAR' # (either 'POS' or 'SHEAR' or 'PROXYSHEAR')
 fits_handler = FitsProcessor()
 
 # to display the contents of a fits file
-fits_handler.display_contents(input_path=input_fits_path)
+fits_handler.display_contents(input_fits_path=input_fits_path)
 
 # to generate the catalog
 fits_handler.generate_catalog(type=catalog_type, input_fits_path=input_fits_path, output_path=output_dir, display_output=True)
